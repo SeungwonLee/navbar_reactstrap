582f06911865433b40d7c48633eb5c3f268ffe7b
seungwon <zolaman2002@naver.com>
2020-09-09 23:57:24 +0900

    react strap router test

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 import './index.css';
+import Main from './Main/Main';
 import Header from './header/Header';
 import App from './app/App';
 import Body from './body/Body';
 import Footer from './footer/Footer';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-    <Header />
-    <Body />
-    <Footer />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render((
+  <BrowserRouter>
+    <Main />
+  </BrowserRouter>
+), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
