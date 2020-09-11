c1e6fb450d144024667d5f92eecdd36f452c8747
seungwon <zolaman2002@naver.com>
2020-09-11 16:55:38 +0900

    added div

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -5,17 +5,18 @@ import { Badge } from 'reactstrap';
 const Work = () => {
   return (
     <div class="container">
-      <p><h5>LINE Android developer</h5></p>
-      <p><div class="title">Overlay stickers</div> </p>
-      <div class="since title-margin">2019</div>
-      <div class="description_title font-weight-bold">Description</div>
-      <div class="description description-margin">Overlay video with stickers</div>
+      <div>
+        <p><h5>LINE Android developer</h5></p>
+        <p><div class="title">Overlay stickers</div> </p>
+        <div class="since title-margin">2019</div>
+        <div class="description_title font-weight-bold">Description</div>
+        <div class="description description-margin">Overlay video with stickers</div>
 
-      <div class="tech font-weight-bold">Used :{' '}
-        <Badge color="secondary">Kotlin</Badge>{' '}
-        <Badge color="secondary">OpenGL ES</Badge>
+        <div class="tech font-weight-bold">Used :{' '}
+          <Badge color="secondary">Kotlin</Badge>{' '}
+          <Badge color="secondary">OpenGL ES</Badge>
+        </div>
       </div>
-
     </div>
   );
 }
